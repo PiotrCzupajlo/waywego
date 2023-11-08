0942718026d6ff5c1afb920ed8a7537d2704aa47
PiotrCzupajlo <piotrczupajlobusiness@wp.pl>
2023-11-08 17:57:22 +0100

    some styles to desktop login page

--- waywego/Desktop/DesktopLandingPage.xaml.cs
+++ waywego/Desktop/DesktopLandingPage.xaml.cs
@@ -1,6 +1,6 @@
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.IO;
 using waywego.Viewmodel;
-
 
 namespace waywego.Desktop;
 
@@ -10,5 +10,41 @@ public partial class DesktopLandingPage : ContentPage
 	{
 		InitializeComponent();
 		this.BindingContext = new StartPageViewModel(this.Navigation);
+
+		resize();
+		changeimages();
+
+	}
+
+
+
+
+
+	// method resize: Resize the window to fullscreen and locks it size
+	public async void resize() {
+		await Task.Delay(10);
+        double left = (DeviceDisplay.Current.MainDisplayInfo.Width / 5) * 2;
+        double right = DeviceDisplay.Current.MainDisplayInfo.Width - left;
+        leftside.WidthRequest = left;
+        rightside.WidthRequest = right;
+		Window.MinimumWidth = DeviceDisplay.Current.MainDisplayInfo.Width;
+		Window.MinimumHeight = DeviceDisplay.Current.MainDisplayInfo.Height;
+		rightimage.MaximumWidthRequest = right;
+		rightimage.MaximumHeightRequest = DeviceDisplay.Current.MainDisplayInfo.Height*0.8;
+		
+    }
+	// change the images on the page
+	public async void changeimages() {
+		await Task.Delay(20);
+		//pozyskiwanie patha
+		string path = AppDomain.CurrentDomain.BaseDirectory;
+		int index = path.IndexOf("\\bin\\");
+		string s = path.Substring(0, index);
+		string imagepath = s + "\\Images\\eiffel.png";
+
+        rightimage.Source = ImageSource.FromFile(imagepath);
+
+
+
 	}
 }
