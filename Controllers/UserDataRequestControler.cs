5a71e9c009e19618b0623da23c463fba812ec8ce
PiotrCzupajlo <piotrczupajlobusiness@wp.pl>
2023-12-07 19:35:15 +0100

    login endpoint

--- Controllers/UserDataRequestControler.cs
+++ Controllers/UserDataRequestControler.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using waywegoapi.Models;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Data.SqlTypes;
+using Microsoft.Data.SqlTypes;
 
 namespace waywegoapi.Controllers
 {
@@ -7,12 +13,31 @@ namespace waywegoapi.Controllers
     [ApiController]
     public class UserDataRequestControler : ControllerBase
     {
+        public string cs = "Data Source=DESKTOP-GN12VV1\\SQLEXPRESS01;Initial Catalog=waywego;Integrated Security=True;TrustServerCertificate=True";
         [HttpGet]
-        public IEnumerable<User> GetUsers() { 
-        User user = new User();
-        User user1 = new User();
+        public async Task<ActionResult<bool>> GetUsers(string username,string password) {
+            DataTable dt = new DataTable();
+            string base64username = Convert.ToBase64String(Encoding.UTF8.GetBytes(username));
+            string base64password = Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
+
+            string sql = "select count(id) as ids from Users where username=\'" + base64username + "\' and password =\'" + base64password + "\';";
+            using (SqlConnection conn = new SqlConnection(cs))
+            { 
+                await conn.OpenAsync();
+                SqlDataAdapter sda = new SqlDataAdapter(sql,conn);
+                sda.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    var val = dr["ids"];
+                    if (Convert.ToInt32(val)>0)
+                    {
+                        return Ok(true);
+                    }
+                }
+            
+            }
+            return Ok(false);
         
-            return new List<User> { user, user1 };
 
         }
 
